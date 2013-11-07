a9bfaaf7e767b2f094c8eea6384eedf652f0f248
Riccardo Cagnasso <riccardo@phascode.org>
2013-11-07 18:28:09 +0100

    StringResponse

---
+++ NetFluid III/Responses/StringResponse.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace NetFluid
+{
+	public class StringResponse : IResponse
+	{
+		private IEnumerable<string> strings;
+		private bool flusheveryline;
+
+		public StringResponse(string str, bool flusheveryline=false)
+		{
+			this.strings = new List<string> { str };
+			this.flusheveryline = flusheveryline;
+		}
+
+		public StringResponse (IEnumerable<string> strings, bool flusheveryline=false)
+		{
+			this.strings = strings;
+			this.flusheveryline = flusheveryline;
+		}
+
+		public void SendResponse (Context cnt)
+		{
+			cnt.SendHeaders ();
+			foreach (string s in this.strings) 
+			{
+				cnt.Writer.Write(s);
+				if(this.flusheveryline)
+				{
+					cnt.Writer.Flush();
+				}
+			}
+        }
+	}
+}
+
