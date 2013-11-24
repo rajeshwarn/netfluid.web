09b841cd11084ac1f941f2e70ed59957d4e08e6d
Matteo Fabbri <matteo@phascode.org>
2013-11-24 18:21:55 +0100

    Started asp support

---
+++ NetFluid.Site/Articles/ArticleManager.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using NetFluid;
+using System;
+
+
+namespace NetFluidService
+{
+    public class ArticleManager:FluidPage
+    {
+        [ParametrizedRoute("/delete")]
+        public FluidTemplate Delete(Article article)
+        {
+            if (article == null)
+                return new FluidTemplate("./UI/index.html");
+
+            var user = Session<User>("user");
+
+            if (user != null && user.Admin)
+                Article.Delete(article);
+
+            return new FluidTemplate("./Articles/UI/read.html", article);
+        }
+
+        [Route("/approve")]
+        public FluidTemplate Approve(Article article)
+        {
+            if (article == null)
+                return new FluidTemplate("./UI/index.html");
+
+            var user = Session<User>("user");
+
+            if (user!=null && user.Admin)
+            {
+                article.Approved = true;
+                Article.Save(article);
+            }
+            return new FluidTemplate("./Articles/UI/read.html", article);
+        }
+
+        [ParametrizedRoute("/read")]
+        public FluidTemplate Read(Article article)
+        {
+            if (article==null)
+                return new FluidTemplate("./UI/index.html");
+
+            if (Session(article.Link)==null)
+            {
+                Session(article.Link,DateTime.Now);
+                article.Views++;
+                Article.Save(article);
+            }
+            return new FluidTemplate("./Articles/UI/read.html",article);
+        }
+
+        [ParametrizedRoute("/write")]
+        public FluidTemplate Write(Article article)
+        {
+            if (Session("user") == null)
+                return new FluidTemplate("./Users/UI/SignIn.html");
+
+            return new FluidTemplate("./Articles/UI/write.html",article);
+        }
+
+        [Route("/edit")]
+        public FluidTemplate Edit(string title, string link, string category, string @abstract, string body)
+        {
+            User user = Session("user");
+            if (user == null)
+                return new FluidTemplate("./Users/UI/SignIn.html");
+
+            var article = Article.Parse(link);
+
+            if (article == null)
+                return new FluidTemplate("./UI/index.html");
+
+            if (article.Author==user.Name || user.Admin)
+            {
+                article.Title = title;
+                article.Category = category;
+                article.Abstract = @abstract;
+                article.Body = body;
+            }
+
+            Article.Save(article);
+
+            return new FluidTemplate("./Articles/UI/read.html", article);
+        }
+
+        [Route("/save")]
+        public FluidTemplate Save(string title, string link,string category,string @abstract,string body)
+        {
+            User user = Session("user");
+            if (user == null)
+                return new FluidTemplate("./Users/UI/SignIn.html");
+
+            var attachments = new List<Attachment>();
+            var dir = Path.GetFullPath("./attachments");
+            
+            foreach (var f in Files)
+            {
+                var name = f.FileName;
+                var p = Path.GetFullPath(Path.Combine(dir, f.Name));
+
+                if (!p.StartsWith(dir))
+                    continue;
+
+                while (File.Exists(p))
+                {
+                    name = Security.UID() + f.Extension;
+                    p = Path.GetFullPath(Path.Combine(dir, name));
+                }
+
+                f.SaveAs(p);
+                attachments.Add(new Attachment { FileName = f.FileName, Name = name });
+            }
+
+            var article = new Article
+            {
+                Abstract = @abstract,
+                Approved = user.Admin,
+                Author = Session<User>("user").Name,
+                Body = body,
+                Category = category,
+                DateTime = DateTime.Now,
+                Link = link,
+                Title = title,
+                Views = 0,
+                Attachment = attachments.ToArray()
+            };
+
+            Article.Save(article);
+
+            return new FluidTemplate("./Articles/UI/read.html",article);
+        }
+    }
+}
---
+++ NetFluid.Site/Users/User.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+
+namespace NetFluidService
+{
+    [Serializable]
+    public class User
+    {
+        [BsonId]
+        public ObjectId _id { get; set; }
+
+        public string Name { get; set; }
+
+        public string NiceName { get; set; }
+
+        public string Password { get; set; }
+
+        public UserStatus Status { get; set; }
+
+        public string Avatar { get; set; }
+
+        public string Mail { get; set; }
+
+        [Serializable]
+        public enum UserStatus
+        {
+            InActive,
+            Active,
+            Banned,
+            Admin
+        }
+
+        [NonSerialized]
+        static readonly MongoDatabase db;
+
+        static User()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var server = client.GetServer();
+            db = server.GetDatabase("netfluid");
+
+            if (Collection.FindOne()==null)
+            {
+                var anon = new User { Name = "anonymous", NiceName = "Anonymous", Mail = "anon@netfluid.org"};
+                var nf = new User { Name = "netfluid", NiceName = "NetFluid", Mail ="netfluid@netfluid.org", Status = User.UserStatus.Admin, Password = "ludmilla" };
+
+                Collection.Save(anon);
+                Collection.Save(nf);
+            }
+        }
+
+        static MongoCollection<User> Collection
+        {
+            get { return db.GetCollection<User>("User"); }
+        }
+
+        public User()
+        {
+            Name = string.Empty;
+            NiceName = string.Empty;
+            Password = string.Empty;
+            Avatar = "default.png";
+        }
+
+        public override string ToString()
+        {
+            return NiceName;
+        }
+
+        public bool IsActive
+        {
+            get { return  Status == UserStatus.Active || Status == UserStatus.Admin;}
+        }
+
+        public bool Banned
+        {
+            get { return  Status == UserStatus.Banned;}
+        }
+
+        public bool Admin
+        {
+            get { return  Status == UserStatus.Admin;}
+        }
+
+        public void ChangePassword(string newpwd)
+        {
+            this.Password = NetFluid.Security.Sha1(newpwd);
+            Collection.Save(this);
+        }
+
+        public static bool IsBanned(User usr)
+        {
+            return usr != null && usr.Status == UserStatus.Banned;
+        }
+
+        public static bool IsAdmin(User usr)
+        {
+            return usr != null && usr.Status == UserStatus.Admin;
+        }
+
+        public static void Save(User user)
+        {
+            Collection.Save(user);
+        }
+
+        public static User Parse(string name)
+        {
+            return Collection.FindOne(Query.EQ("Name",name.ToLowerInvariant()));
+        }
+
+        public static bool Exist(string name)
+        {
+            return Collection.FindOne(Query.EQ("Name", name)) != null;
+        }
+
+        public static User ByMail(string mail)
+        {
+            return Collection.FindOne(Query.EQ("Mail", mail));
+        }
+
+        public static User Validate(string username, string password)
+        {
+            if (password == null)
+                password = string.Empty;
+
+            if (username == null)
+                username = string.Empty;
+
+            if (username == "anonymous")
+                return Parse("anonymous");
+
+            return Collection.FindOne(Query.And(Query.EQ("Name", username),Query.EQ("Password",password)));
+        }
+    }
+}
