803edb61d81f629eb77b899787aa2b06e551ee74
Matteo Fabbri <matteo@phascode.org>
2013-11-30 20:11:06 +0100

    Added csv serialization

---
+++ NetFluid/Responses/CSVResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetFluid
+{
+    public class CSVResponse<T> : IResponse
+    {
+        public char TextQualifier;
+        public char FieldSeparator;
+        public IEnumerable<T> Collection;
+
+        public CSVResponse(IEnumerable<T> collection)
+        {
+            Collection = collection;
+            FieldSeparator = ';';
+            TextQualifier = '"';
+        }
+
+        public void SendResponse(Context cnt)
+        {
+            cnt.Response.ContentType = "application/csv";
+            cnt.SendHeaders();
+            CSV.Serialize(Collection, cnt.Writer, FieldSeparator, TextQualifier);
+            cnt.Writer.Flush();
+            cnt.Close();
+        }
+    }
+}
