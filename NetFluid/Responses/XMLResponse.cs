f11ce88ed20ae555aa6774eb0995559af22c82a3
Matteo Fabbri <matteo@phascode.org>
2013-12-09 21:43:13 +0100

    improved repsonses

---
+++ NetFluid/Responses/XMLResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetFluid
+{
+    public class XMLResponse:IResponse
+    {
+        object obj;
+
+        public XMLResponse(object obj)
+        {
+            this.obj = obj;
+        }
+
+        public void SendResponse(Context cnt)
+        {
+            cnt.Response.ContentType = "application/xml";
+            cnt.SendHeaders();
+
+            obj.ToXML(cnt.OutputStream);
+        }
+    }
+}
