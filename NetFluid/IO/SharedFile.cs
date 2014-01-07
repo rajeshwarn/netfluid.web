faff790693ea5441d1bb0777025f86c425855502
Matteo Fabbri <matteo@phascode.org>
2014-01-07 18:46:19 +0100

    bug fix

---
+++ NetFluid/IO/SharedFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace NetFluid.IO
+{
+    public class SharedFile
+    {
+        Timer timer;
+        Thread thread;
+
+        string path;
+
+        long position;
+        Stream writer;
+
+        public SharedFile(string path)
+        {
+            this.path = Path.GetFullPath(path);
+            writer = new FileStream(path, FileMode.Append, FileAccess.ReadWrite, FileShare.ReadWrite);
+        }
+
+        public void Write(byte[] payload)
+        {
+        }
+
+        public void Write(string str)
+        {
+            Write(Encoding.UTF8.GetBytes(str));
+        }
+
+        public void WriteLine(string str)
+        {
+            Write(Encoding.UTF8.GetBytes(str+"\r\n"));
+        }
+    }
+}
