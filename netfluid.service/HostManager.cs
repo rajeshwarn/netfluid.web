720d6d2529aeb05ea74879115587d5759abe2e12
Matteo Fabbri <matteo@phascode.org>
2014-05-04 15:19:35 +0200

    host managment

--- netfluid.service/HostManager.cs
+++ netfluid.service/HostManager.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NetFluid.Service
+﻿namespace NetFluid.Service
 {
     public class HostManager:FluidPage
     {
@@ -12,6 +6,38 @@ namespace NetFluid.Service
         public IResponse Home()
         {
             return new FluidTemplate(Context.IsLocal ? "./UI/admin.html": "./UI/index.html");
+        }
+
+        [ParametrizedRoute("/start")]
+        public IResponse Start(string name)
+        {
+            if (Context.IsLocal)
+            {
+                Service.Start(name);
+                return new FluidTemplate("./UI/admin.html");
+            }
+            return new FluidTemplate("./UI/index.html");
+        }
+        [ParametrizedRoute("/stop")]
+        public IResponse Stop(string name)
+        {
+            if (Context.IsLocal)
+            {
+                Service.Stop(name);
+                return new FluidTemplate("./UI/admin.html");
+            }
+            return new FluidTemplate("./UI/index.html");
+        }
+
+        [ParametrizedRoute("/restart")]
+        public IResponse ResStart(string name)
+        {
+            if (Context.IsLocal)
+            {
+                Service.ReStart(name);
+                return new FluidTemplate("./UI/admin.html");
+            }
+            return new FluidTemplate("./UI/index.html");
         }
     }
 }
