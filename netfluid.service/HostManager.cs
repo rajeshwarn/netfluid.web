e054a628c9d7773f04a3c33622efbe6bb8744084
Matteo Fabbri <matteo@phascode.org>
2014-05-05 02:07:38 +0200

    clenaing

--- netfluid.service/HostManager.cs
+++ netfluid.service/HostManager.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace NetFluid.Service
 {
@@ -14,40 +15,37 @@ namespace NetFluid.Service
         [ParametrizedRoute("/start")]
         public IResponse Start(string name)
         {
-            if (Context.IsLocal)
-            {
-                Service.Start(name);
-                return new FluidTemplate("./UI/admin.html");
-            }
-            return new FluidTemplate("./UI/index.html");
+            if (!Context.IsLocal) return new FluidTemplate("./UI/index.html");
+
+            Service.Start(name);
+            return new FluidTemplate("./UI/admin.html");
         }
         
         [ParametrizedRoute("/stop")]
         public IResponse Stop(string name)
         {
-            if (Context.IsLocal)
-            {
-                Service.Stop(name);
-                return new FluidTemplate("./UI/admin.html");
-            }
-            return new FluidTemplate("./UI/index.html");
+            if (!Context.IsLocal) return new FluidTemplate("./UI/index.html");
+
+            Service.Stop(name);
+            return new FluidTemplate("./UI/admin.html");
         }
 
         [ParametrizedRoute("/restart")]
         public IResponse ReStart(string name)
         {
-            if (Context.IsLocal)
-            {
-                Service.ReStart(name);
-                return new FluidTemplate("./UI/admin.html");
-            }
-            return new FluidTemplate("./UI/index.html");
+            if (!Context.IsLocal) return new FluidTemplate("./UI/index.html");
+
+            Service.ReStart(name);
+            return new FluidTemplate("./UI/admin.html");
         }
 
         [Route("/update")]
         public IResponse Update(string id, string name, string application, string hosts, string endpoint, bool enabled)
         {
-            
+            if (!Context.IsLocal) return new FluidTemplate("./UI/index.html");
+
+            Request.Values.ForEach(x=>Console.WriteLine(x.ToString()));
+            return null;
         }
     }
 }
