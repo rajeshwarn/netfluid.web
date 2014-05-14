47e80227ba8305d80563f26bc96574e72f718649
Matteo Fabbri <matteo@phascode.org>
2014-05-14 16:14:03 +0200

    record type parsing

--- NetFluid/Dns/Record.cs
+++ NetFluid/Dns/Record.cs
@@ -41,6 +41,138 @@ namespace NetFluid.DNS
         /// </summary>
         public uint TTL { get; set; }
 
+        /// <summary>
+        /// Return .net type of record from enum RecordType
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static Type Type(RecordType type)
+        {
+            switch (type)
+            {
+                case RecordType.A:
+                    return typeof(RecordA);
+                case RecordType.NS:
+                    return typeof(RecordNS);
+                case RecordType.MD:
+                    return typeof(RecordMD);
+                case RecordType.MF:
+                    return typeof(RecordMF);
+                case RecordType.CNAME:
+                    return typeof(RecordCNAME);
+                case RecordType.SOA:
+                    return typeof(RecordSOA);
+                case RecordType.MB:
+                    return typeof(RecordMB);
+                case RecordType.MG:
+                    return typeof(RecordMG);
+                case RecordType.MR:
+                    return typeof(RecordMR);
+                case RecordType.NULL:
+                    return typeof(RecordNULL);
+                case RecordType.WKS:
+                    return typeof(RecordWKS);
+                case RecordType.PTR:
+                    return typeof(RecordPTR);
+                case RecordType.HINFO:
+                    return typeof(RecordHINFO);
+                case RecordType.MINFO:
+                    return typeof(RecordMINFO);
+                case RecordType.MX:
+                    return typeof(RecordMX);
+                case RecordType.TXT:
+                    return typeof(RecordTXT);
+                case RecordType.RP:
+                    return typeof(RecordRP);
+                case RecordType.AFSDB:
+                    return typeof(RecordAFSDB);
+                case RecordType.X25:
+                    return typeof(RecordX25);
+                case RecordType.ISDN:
+                    return typeof(RecordISDN);
+                case RecordType.RT:
+                    return typeof(RecordRT);
+                case RecordType.NSAP:
+                    return typeof(RecordNSAP);
+                case RecordType.NSAPPTR:
+                    return typeof(RecordNSAPPTR);
+                case RecordType.SIG:
+                    return typeof(RecordSIG);
+                case RecordType.KEY:
+                    return typeof(RecordKEY);
+                case RecordType.PX:
+                    return typeof(RecordPX);
+                case RecordType.GPOS:
+                    return typeof(RecordGPOS);
+                case RecordType.AAAA:
+                    return typeof(RecordAAAA);
+                case RecordType.LOC:
+                    return typeof(RecordLOC);
+                case RecordType.NXT:
+                    return typeof(RecordNXT);
+                case RecordType.EID:
+                    return typeof(RecordEID);
+                case RecordType.NIMLOC:
+                    return typeof(RecordNIMLOC);
+                case RecordType.SRV:
+                    return typeof(RecordSRV);
+                case RecordType.ATMA:
+                    return typeof(RecordATMA);
+                case RecordType.NAPTR:
+                    return typeof(RecordNAPTR);
+                case RecordType.KX:
+                    return typeof(RecordKX);
+                case RecordType.CERT:
+                    return typeof(RecordCERT);
+                case RecordType.A6:
+                    return typeof(RecordA6);
+                case RecordType.DNAME:
+                    return typeof(RecordDNAME);
+                case RecordType.SINK:
+                    return typeof(RecordSINK);
+                case RecordType.OPT:
+                    return typeof(RecordOPT);
+                case RecordType.APL:
+                    return typeof(RecordAPL);
+                case RecordType.DS:
+                    return typeof(RecordDS);
+                case RecordType.SSHFP:
+                    return typeof(RecordSSHFP);
+                case RecordType.IPSECKEY:
+                    return typeof(RecordIPSECKEY);
+                case RecordType.RRSIG:
+                    return typeof(RecordRRSIG);
+                case RecordType.NSEC:
+                    return typeof(RecordNSEC);
+                case RecordType.DNSKEY:
+                    return typeof(RecordDNSKEY);
+                case RecordType.DHCID:
+                    return typeof(RecordDHCID);
+                case RecordType.NSEC3:
+                    return typeof(RecordNSEC3);
+                case RecordType.NSEC3PARAM:
+                    return typeof(RecordNSEC3PARAM);
+                case RecordType.HIP:
+                    return typeof(RecordHIP);
+                case RecordType.SPF:
+                    return typeof(RecordSPF);
+                case RecordType.UINFO:
+                    return typeof(RecordUINFO);
+                case RecordType.UID:
+                    return typeof(RecordUID);
+                case RecordType.GID:
+                    return typeof(RecordGID);
+                case RecordType.UNSPEC:
+                    return typeof(RecordUNSPEC);
+                case RecordType.TKEY:
+                    return typeof(RecordTKEY);
+                case RecordType.TSIG:
+                    return typeof(RecordTSIG);
+                default:
+                    return typeof(RecordUnknown);
+            }
+        }
+
         public static Record FromType(RecordType type)
         {
             switch (type)
