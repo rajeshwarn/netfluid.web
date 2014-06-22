d4562160e5e362dac8ae3fceda119cc66231b06b
Matteo Fabbri <matteo@phascode.org>
2014-06-22 20:07:01 +0200

    cleaning

--- NetFluid.Site/AutoHoster.cs
+++ NetFluid.Site/AutoHoster.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NetFluid.Site
 {
@@ -10,15 +6,13 @@ namespace NetFluid.Site
     {
         public static void Main()
         {
-            Console.WriteLine((DateTime.Now - (new DateTime(2014,3,27))).Days);
             if (!Engine.LoadAppConfiguration())
             {
                 Engine.Logger.Log(LogLevel.Error, "App configuration not loaded, using default values");
                 Engine.DefaultHost.PublicFolderManager.Add("public", "/", "./Public");
-                Engine.Interfaces.AddLoopBack(80);
-                Engine.Interfaces.AddAllAddresses(80);
+                Engine.Interfaces.AddLoopBack();
+                Engine.Interfaces.AddAllAddresses();
             }
-            Engine.DevMode = true;
             Engine.Start();
             Console.ReadLine();
         }
