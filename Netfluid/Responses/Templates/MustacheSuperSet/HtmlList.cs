1bddf5c886e1c22d1c7bed072b5ae781c5551408
Matteo Fabbri <matteo@phascode.org>
2015-09-18 11:34:44 +0200

    moretags !

---
+++ Netfluid/Responses/Templates/MustacheSuperSet/HtmlList.cs
@@ -0,0 +1,35 @@
+﻿using Mustache;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Netfluid.Responses.Templates.MustacheSuperSet
+{
+    internal class HtmlList : InlineTagDefinition
+    {
+        public HtmlList(): base("li")
+        {
+        }
+
+        protected override IEnumerable<TagParameter> GetParameters()
+        {
+            return new TagParameter[]
+            {
+                new TagParameter("object")
+            };
+        }
+
+        public override void GetText(TextWriter writer, Dictionary<string, object> arguments, Scope context)
+        {
+            var source = arguments["object"] as IEnumerable;
+            int count = 0;
+
+            writer.Write("<ul>");
+            foreach (var item in source)
+            {
+                writer.Write("<li>"+count+"</li>");
+            }
+            writer.Write("</ul>");
+        }
+    }
+}
