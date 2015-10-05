c20d3f898ce359e999240bb9be1b90721140b222
Matteo Fabbri <matteo@phascode.org>
2015-10-05 19:33:33 +0200

    added bson class

---
+++ Netfluid/Serialization/Bson.cs
@@ -0,0 +1,99 @@
+﻿using Netfluid.JsonInternals.Bson;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netfluid.Serialization
+{
+    public static class Bson
+    {
+        public static void Serialize(object obj, BinaryWriter writer)
+        {
+            var w = new BsonWriter(writer);
+            var s = new JsonInternals.JsonSerializer();
+            s.Serialize(w, obj);
+        }
+
+        public static void Serialize(object obj, Stream stream)
+        {
+            var w = new BsonWriter(stream);
+            var s = new JsonInternals.JsonSerializer();
+            s.Serialize(w, obj);
+        }
+
+        public static byte[] Serialize(object obj)
+        {
+            var m = new MemoryStream();
+            var w = new BsonWriter(m);
+            var s = new JsonInternals.JsonSerializer();
+            s.Serialize(w, obj);
+            return m.ToArray();
+        }
+
+        public static object Deserialize(BinaryReader reader)
+        {
+            var r = new BsonReader(reader);
+            var s = new JsonInternals.JsonSerializer();
+            return s.Deserialize(r);
+        }
+
+        public static T Deserialize<T>(BinaryReader reader)
+        {
+            var r = new BsonReader(reader);
+            var s = new JsonInternals.JsonSerializer();
+            return (T)s.Deserialize(r,typeof(T));
+        }
+
+        public static object Deserialize(BinaryReader reader,Type t)
+        {
+            var r = new BsonReader(reader);
+            var s = new JsonInternals.JsonSerializer();
+            return s.Deserialize(r,t);
+        }
+
+        public static object Deserialize(Stream stream)
+        {
+            var r = new BsonReader(stream);
+            var s = new JsonInternals.JsonSerializer();
+            return s.Deserialize(r);
+        }
+
+        public static T Deserialize<T>(Stream stream)
+        {
+            var r = new BsonReader(stream);
+            var s = new JsonInternals.JsonSerializer();
+            return (T)s.Deserialize(r,typeof(T));
+        }
+
+        public static object Deserialize(Stream stream,Type t)
+        {
+            var r = new BsonReader(stream);
+            var s = new JsonInternals.JsonSerializer();
+            return s.Deserialize(r,t);
+        }
+
+        public static object Deserialize(byte[] bytes)
+        {
+            var r = new BsonReader(new MemoryStream(bytes));
+            var s = new JsonInternals.JsonSerializer();
+            return s.Deserialize(r);
+        }
+
+        public static T Deserialize<T>(byte[] bytes)
+        {
+            var r = new BsonReader(new MemoryStream(bytes));
+            var s = new JsonInternals.JsonSerializer();
+            return (T)s.Deserialize(r,typeof(T));
+        }
+
+        public static object Deserialize(byte[] bytes,Type t)
+        {
+            var r = new BsonReader(new MemoryStream(bytes));
+            var s = new JsonInternals.JsonSerializer();
+            return s.Deserialize(r,t);
+        }
+    }
+}
