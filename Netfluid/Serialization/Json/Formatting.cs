c10e2ff166cb73086b8684e09af4b8fd6dfdfdbb
Matteo Fabbri <matteo@phascode.org>
2015-10-05 19:12:06 +0200

    simplyfing

--- Netfluid/Serialization/Json/Formatting.cs
+++ Netfluid/Serialization/Json/Formatting.cs
@@ -23,7 +23,7 @@
 // OTHER DEALINGS IN THE SOFTWARE.
 #endregion
 
-namespace Netfluid.JsonInternals
+namespace Netfluid
 {
     /// <summary>
     /// Specifies formatting options for the <see cref="JsonTextWriter"/>.
