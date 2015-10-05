b39c124b1fa5e83ad467f3a3655abaff36a5f836
Matteo Fabbri <matteo@phascode.org>
2015-10-05 19:15:32 +0200

    unused nmepsace ref

--- Netfluid/Serialization/Json.cs
+++ Netfluid/Serialization/Json.cs
@@ -27,7 +27,6 @@ using System;
 using System.IO;
 using System.Globalization;
 using System.Numerics;
-using System.Threading.Tasks;
 using Netfluid.JsonInternals.Utilities;
 using System.Xml;
 using Netfluid.JsonInternals.Converters;
