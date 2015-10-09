41082f73c494b89d115c74325e9196b410c22808
Matteo Fabbri <matteo@phascode.org>
2015-10-09 19:09:54 +0200

    cleaning

--- Netfluid/Program.cs
+++ Netfluid/Program.cs
@@ -22,21 +22,7 @@ namespace Example
             host.Logger = new Netfluid.Logging.ConsoleLogger(LogLevel.Debug);
             host.PublicFolders.Add(new PublicFolder { RealPath="./Resources", VirtualPath="/cdn" });
             host.Map(typeof(Program));
-            host.StartAsync();
-
-            JObject obj;
-
-            var k = JSON.SerializeObject(new[] {1,2,3,4,5,6,77,8,9,9 });
-            var d = JSON.Deserialize(k);
-
-            Console.WriteLine(d.GetType());
-
-            AppDomain.CurrentDomain.UnhandledException += (s,e)=>
-            {
-                host.Logger.Error(e.ExceptionObject as Exception);
-            };
-
-            while (Console.ReadLine() != "end");
+            host.Start();
         }
 
         [Route("/")]
