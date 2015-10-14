6b41bc050d4de42c72f26ce632d1cd9feb4eb637
Matteo Fabbri <matteo@phascode.org>
2015-10-14 19:22:03 +0200

    cleaning

--- Netfluid/Program.cs
+++ Netfluid/Program.cs
@@ -8,23 +8,8 @@ namespace Example
 {
     class Program
     {
-        public string name;
-
-
         static void Main(string[] args)
         {
-            var alfa = "qwertyuiopasdfghjklzxcvbnm1234567890";
-
-            var k = new KeyValueStore<Program>("ciao");
-
-            for (int i = 1; i < 455555; i++)
-            {
-                k.Insert(new string(alfa.Random(128).ToArray()), new Program());
-                if (i % 2000 == 0) Console.WriteLine("LOADING " + i);
-            }
-
-            Console.WriteLine("SUCA");
-
             var host = new NetfluidHost("*");
             host.Logger = new Netfluid.Logging.ConsoleLogger(LogLevel.Debug);
             host.PublicFolders.Add(new PublicFolder { RealPath="./Resources", VirtualPath="/cdn" });
