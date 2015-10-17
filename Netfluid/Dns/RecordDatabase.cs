22f42b2e661c7334fef6b3794153b1db1676b12c
Matteo Fabbri <matteo@phascode.org>
2015-10-17 20:23:25 +0200

    removed program

--- Netfluid/Dns/RecordDatabase.cs
+++ Netfluid/Dns/RecordDatabase.cs
@@ -14,10 +14,6 @@ namespace Netfluid.Dns
 
         RecordStorage Storage;
         Tree<string, uint> PrimaryIndex;
-
-        ReaderWriterLockSlim storeLocker;
-        ReaderWriterLockSlim indexLocker;
-
         public long Count { get; private set; }
 
         public RecordDatabase(string path)
@@ -32,20 +28,7 @@ namespace Netfluid.Dns
 
             PrimaryIndex = new Tree<string, uint>(new TreeDiskNodeManager<string, uint>(new TreeStringSerialzier(), new TreeUIntSerializer(), new RecordStorage(new BlockStorage(this.primaryIndexFile, 4096))), true);
 
-            storeLocker = new ReaderWriterLockSlim();
-            indexLocker = new ReaderWriterLockSlim();
-
             Count = PrimaryIndex.LargerThanOrEqualTo("").Count();
-        }
-
-        public IEnumerable<string> ByDomain(string domain)
-        {
-            return PrimaryIndex.EqualTo(domain).Select(x=>x.Item1);
-        }
-
-        public void add(string key,uint val)
-        {
-            PrimaryIndex.Insert(key, val);
         }
     }
 }
