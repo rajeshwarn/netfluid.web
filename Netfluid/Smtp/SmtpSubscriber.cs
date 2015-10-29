19e92c42c740a256d62a67294101dd4586782c6d
Matteo Fabbri <matteo@phascode.org>
2015-10-29 20:18:25 +0100

    UPGRADE

---
+++ Netfluid/Smtp/SmtpSubscriber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Netfluid.Smtp
+{
+    public static class SmtpSubscriber
+    {
+        static Dictionary<string, Action<SmtpSessionInfo>> subscribers;
+
+        static SmtpSubscriber()
+        {
+            subscribers = new Dictionary<string, Action<SmtpSessionInfo>>();
+        }
+
+        public static void Add(string host, Action<SmtpSessionInfo> action)
+        {
+            subscribers[host] = action;
+        }
+
+        public static void NewSession(SmtpSessionInfo s)
+        {
+            foreach (var r in s.Recipients)
+            {
+                if (subscribers.ContainsKey(r.Host))
+                    subscribers[r.Host](s);
+            }
+        }
+    }
+}
