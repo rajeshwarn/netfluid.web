c29a4895e1777b5724f9d1aae0c233000f343c06
Matteo Fabbri <matteo@phascode.org>
2015-11-13 08:06:18 +0100

    untracked smtp dispatcher

---
+++ Netfluid/Smtp/SmtpDispatcher.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net;
+using System.Threading;
+
+namespace Netfluid.Smtp
+{
+    public class SmtpDispatcher
+    {
+        Dictionary<string, string> hostToUrl;
+        public SmtpServer Server { get; private set; }
+
+        public SmtpDispatcher()
+        {
+            hostToUrl = new Dictionary<string, string>();
+            Server = new SmtpServer();
+            Server.OnMessageArrived = (x) => 
+            {
+                var json = JSON.Serialize(x);
+                var wc = new WebClient();
+
+                foreach (var to in x.To)
+                {
+                    string url;
+
+                    if (hostToUrl.TryGetValue(to.Host,out url))
+                    {
+                        wc.UploadString(url, json);
+                    }
+                }
+                return DateTime.Now.Ticks.ToString();
+            };
+        }
+
+        public static void Register(string registrant, string url, string host)
+        {
+            var wc = new WebClient();
+
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    wc.UploadValues(registrant, new NameValueCollection
+                    {
+                        { "url", url },
+                        { "host", host }
+                    });
+                    break;
+                }
+                catch (Exception)
+                {
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
